a810e04531a2f0f02db3fd352770176dcb7a32a0
octaviodegodoy <octaviodegodoy@gmail.com>
2019-03-28 10:10:47 -0300

    passport jwt

---
+++ services/passport.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const jwtOptions = {};
+
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    User.findById(payload.sub, function(err, user) {
+        if (err) { return done(err, user); }
+        
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    });
+});
