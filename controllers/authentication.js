c9f95f9bfdbc9d0cb9bf05bafedd44c5ce03a187
octaviodegodoy <octaviodegodoy@gmail.com>
2019-03-27 22:28:48 -0300

    many things

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -5,15 +5,39 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if (!email || !password) {
+        res.status(422).send({ error: " You should provide email and password !!! "});
+    }
+
     User.findOne({ email: email}, function(err, existingUser) {
+
+        if(err){return next(err);}
+         // If an user with the same email exists, return an error
+
+        if(existingUser) {
+            return res.status(422).send({error: 'Email in use '});
+        }
+
+           
+    // If a user with email does NOT exist, create and save user record
+
+    const user = new User({
+        email: email,
+        password: password
+    });
+
+    user.save(function (err){
+        if(err) { return next(err); }
+
+        res.json({ success: true });
+    });
+    // Respond to request saying the user was created
+
+    
 
     });
 
-    // See if the user with the given email exists
+    
 
-    // If an user with the same email exists, return an error
 
-    // If a user with email does NOT exist, create and save user record
-
-    // Respond to request saying the user was created
 }
