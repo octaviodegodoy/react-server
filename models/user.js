c8fc21d2a318fd1eeee964411497f5b9872bb8ed
octaviodegodoy <octaviodegodoy@gmail.com>
2019-03-28 09:40:20 -0300

    more authentication stuff

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 
 // define the model
@@ -7,6 +8,24 @@ const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
 });
+
+// On save hook, encrypt pasword
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err); }
+        
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) {return next(err);}
+
+            user.password = hash;
+            
+            next();
+        });
+    });
+});
+
 // create the model class
 const ModelClass = mongoose.model('user',userSchema);
 // export the model class
