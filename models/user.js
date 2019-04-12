784e7eaba7410db75e85db1dd292783fa2744ed9
octaviodegodoy <octaviodegodoy@gmail.com>
2019-04-12 20:49:32 -0300

    Adding encryption

--- models/user.js
+++ models/user.js
@@ -26,6 +26,15 @@ userSchema.pre('save', function(next) {
     });
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) { return callback(); }
+
+        callback(null, isMatch);
+
+    });
+}
+
 // create the model class
 const ModelClass = mongoose.model('user',userSchema);
 // export the model class
--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,16 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+const localOptions = { usernameField: 'email' }
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+     User.findOne({ email: email}, function(err, user) {
+         if(err) { return done(err); }
+
+         if(!user) { return done(null, false); }
+     });
+}); 
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: config.secret 
